424c364c7c81b196f3354f5466098ac196e54ec8
PanMour <115315282+PanMour@users.noreply.github.com>
2022-12-01 13:21:08 +0200

    Add files via upload

--- info.lua
+++ info.lua
@@ -3,12 +3,12 @@ function Image(img)
       local f = io.open("project/" .. img.src, 'r')
       local doc = pandoc.read(f:read('*a'))
       f:close()
+      local title = pandoc.utils.stringify(doc.meta.title)
       local caption = pandoc.utils.stringify(doc.meta.caption) 
       local name = pandoc.utils.stringify(doc.meta.name)
       local am = pandoc.utils.stringify(doc.meta.am)
       local line = "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-      local content = '> **Addition Information:** \n ' .. caption .. '  \n>'  .. line .. '  \n ' .. "Ονοματεπώνυμο: " .. name .. '  \n '  .. "Αριθμός Μητρώου: " .. am .. ' \n ' ..  line
+      local content = '> **My Addition:** \n ' .. title .. ":" ..  ' \n ' .. caption .. '  \n>'  .. line .. '  \n ' .. "Ονοματεπώνυμο: " .. name .. '  \n '  .. "Αριθμός Μητρώου: " .. am .. ' \n ' ..  line
       return pandoc.RawInline('markdown',content)
     end
 end
-
